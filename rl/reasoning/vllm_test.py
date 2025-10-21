f356190cb3d1b96b77a39a7418e0d23c8ee4ece9
ak-runpod <ak3ka@virginia.edu>
2025-10-21 19:21:30 +0000

    h100 clean run basic vllm code

--- rl/reasoning/vllm_test.py
+++ rl/reasoning/vllm_test.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import sys
 import grader.drgrpo_grader
 
 def getPromptTemplate (prompt_filepath):
@@ -15,28 +16,29 @@ if __name__ == '__main__':
     prompt_template = getPromptTemplate (prompt_filepath)
 
     base_prompts = ["Compute 12 + 34*2 and explain.", "I have two apples today. I will eat one tomorrow. How many will I have the day after tomorrow?"]
-    base_prompts = [base_prompts[1]]
     prompts = [prompt_template.format(question=ele) for ele in base_prompts]
 
 
 
     from vllm import LLM, SamplingParams
 
-
-    # small model recommended for CPU trials
-    model_id = "Qwen/Qwen2.5-Math-1.5B"  # or another tiny HF model
+    model_id = "Qwen/Qwen2.5-Math-1.5B"  
 
     llm = LLM(
         model=model_id,
-        dtype="float16",   
-        max_num_seqs=1
+        #dtype="float16",   
+        max_num_seqs=8
     )
 
     
-    sampling_params = SamplingParams(max_tokens=2048, temperature=1.0, top_p = 1.0, stop=["</answer>"])
+    sampling_params = SamplingParams(max_tokens=16384, temperature=1.0, top_p = 1.0, stop=["</answer>"])
     sampling_params.include_stop_str_in_output = True #</answer> will be incl. in generation
     outputs = llm.generate(prompts, sampling_params)
 
-    for o in outputs:
-        print ("-----------"*3)
-        print(o.outputs[0].text)
+
+    try :
+        for o in outputs:
+            print ("-----------"*3)
+            print(o.outputs[0].text)
+    finally:
+        del llm
