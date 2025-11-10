59a998e4247a5a08921648ffb7bdcc5411f5756a
avkalyan <avkalyan@cisco.com>
2025-11-09 18:31:22 -0800

    changing freq of validation

--- rl/reasoning/sft/run.py
+++ rl/reasoning/sft/run.py
@@ -141,7 +141,7 @@ if __name__ == '__main__':
 
             
         #Run validation test
-        if (epoch + 1)% 10 == 0:
+        if (epoch + 1)% 2 == 0:
             #1. Copy current-sft weights to VLLMs GPU (device=cuda:1)
             vllm_helper.load_policy_into_vllm_instance (model, vllm_valdn_model)
 
