7b9464e5567da6988dece67b02442add68ee40f2
avkalyan <avkalyan@cisco.com>
2025-11-13 21:42:07 -0800

    doing an end of run commit

--- rl/reasoning/sft/run.py
+++ rl/reasoning/sft/run.py
@@ -207,5 +207,6 @@ if __name__ == '__main__':
 
 
 
-    #output_model_path = f"sft_model_bs{train_batch_size*grad_acc_steps}_lr{learning_rate}"
-    #model.save_pretrained(output_model_path)
+    output_model_path = f"sft_model_bs{train_batch_size*grad_acc_steps}_lr{learning_rate}"
+    model.save_pretrained(output_model_path)
+    tokenizer.save_pretrained(output_model_path)
